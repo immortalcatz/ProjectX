561b217e8154103771d1884298287288c213f999
KitsuneAlex <kitsune-alex@gmx.de>
2017-04-09 19:24:23 +0200

    New resource textures \o/

--- src/main/java/keri/projectx/util/ModPrefs.java
+++ src/main/java/keri/projectx/util/ModPrefs.java
@@ -3,7 +3,7 @@ package keri.projectx.util;
 public class ModPrefs {
 
     public static final String MODID = "projectx";
-    public static final String NAME = "ProjectX";
+    public static final String NAME = "ProjectX 2";
     public static final String VERSION = "2.0.2";
     public static final String DEPS = "required-after:ninetaillib";
     public static final String ACC_MC = "1.10.2";
