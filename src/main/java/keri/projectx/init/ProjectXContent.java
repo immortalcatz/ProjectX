a05dc4db4731268de0c1f9c958aa22df4eb44f16
KitsuneAlex <kitsune-alex@gmx.de>
2017-04-10 13:45:06 +0200

    NTL update & armor textures

--- src/main/java/keri/projectx/init/ProjectXContent.java
+++ src/main/java/keri/projectx/init/ProjectXContent.java
@@ -1,11 +1,9 @@
 package keri.projectx.init;
 
-import keri.ninetaillib.item.ItemArmorHelper;
 import keri.projectx.item.*;
 import keri.projectx.util.ModPrefs;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class ProjectXContent {
@@ -23,8 +21,7 @@ public class ProjectXContent {
     public static Item[] xycroniumToolsDark;
     public static Item[] xycroniumToolsLight;
 
-    private static ItemArmor.ArmorMaterial armorMaterialXycronium = EnumHelper.addArmorMaterial("xycronium", ModPrefs.MODID + ":xycronium", 16, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.1F);
-    private static ItemArmorHelper armorHelper = new ItemArmorHelperProjectX();
+    private static ItemArmorHelperProjectX armorHelper = new ItemArmorHelperProjectX();
     public static Item[] xycroniumArmorBlue;
     public static Item[] xycroniumArmorGreen;
     public static Item[] xycroniumArmorRed;
@@ -37,17 +34,17 @@ public class ProjectXContent {
         xycroniumNugget = new ItemXycroniumNugget();
         xycroniumDust = new ItemXycroniumDust();
 
-        xycroniumToolsBlue = toolHelper.createToolset(toolMaterialXycronium);
-        xycroniumToolsGreen = toolHelper.createToolset(toolMaterialXycronium);
-        xycroniumToolsRed = toolHelper.createToolset(toolMaterialXycronium);
-        xycroniumToolsDark = toolHelper.createToolset(toolMaterialXycronium);
-        xycroniumToolsLight = toolHelper.createToolset(toolMaterialXycronium);
+        xycroniumToolsBlue = toolHelper.createToolset("xycronium_blue", toolMaterialXycronium);
+        xycroniumToolsGreen = toolHelper.createToolset("xycronium_green", toolMaterialXycronium);
+        xycroniumToolsRed = toolHelper.createToolset("xycronium_red", toolMaterialXycronium);
+        xycroniumToolsDark = toolHelper.createToolset("xycronium_dark", toolMaterialXycronium);
+        xycroniumToolsLight = toolHelper.createToolset("xycronium_light", toolMaterialXycronium);
 
-        xycroniumArmorBlue = armorHelper.createArmorSet("xycronium_blue", armorMaterialXycronium);
-        xycroniumArmorGreen = armorHelper.createArmorSet("xycronium_green", armorMaterialXycronium);
-        xycroniumArmorRed = armorHelper.createArmorSet("xycronium_red", armorMaterialXycronium);
-        xycroniumArmorDark = armorHelper.createArmorSet("xycronium_dark", armorMaterialXycronium);
-        xycroniumArmorLight = armorHelper.createArmorSet("xycronium_light", armorMaterialXycronium);
+        xycroniumArmorBlue = armorHelper.createArmorSet("xycronium_blue", EnumHelper.addArmorMaterial("xycronium_blue", ModPrefs.MODID + ":xycronium_blue", 16, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.1F));
+        xycroniumArmorGreen = armorHelper.createArmorSet("xycronium_green", EnumHelper.addArmorMaterial("xycronium_green", ModPrefs.MODID + ":xycronium_green", 16, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.1F));
+        xycroniumArmorRed = armorHelper.createArmorSet("xycronium_red", EnumHelper.addArmorMaterial("xycronium_red", ModPrefs.MODID + ":xycronium_red", 16, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.1F));
+        xycroniumArmorDark = armorHelper.createArmorSet("xycronium_dark", EnumHelper.addArmorMaterial("xycronium_dark", ModPrefs.MODID + ":xycronium_dark", 16, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.1F));
+        xycroniumArmorLight = armorHelper.createArmorSet("xycronium_light", EnumHelper.addArmorMaterial("xycronium_light", ModPrefs.MODID + ":xycronium_light", 16, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.1F));
     }
 
     public static void init(){
