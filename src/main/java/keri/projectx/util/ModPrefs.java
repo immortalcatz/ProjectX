817471026d2029bf8a31bf2c96af332ac2b7f8ea
KitsuneAlex <kitsune-alex@gmx.de>
2017-04-10 13:46:28 +0200

    Test Build 3

--- src/main/java/keri/projectx/util/ModPrefs.java
+++ src/main/java/keri/projectx/util/ModPrefs.java
@@ -4,7 +4,7 @@ public class ModPrefs {
 
     public static final String MODID = "projectx";
     public static final String NAME = "ProjectX 2";
-    public static final String VERSION = "2.0.2";
+    public static final String VERSION = "2.0.3";
     public static final String DEPS = "required-after:ninetaillib";
     public static final String ACC_MC = "1.10.2";
     public static final String CSIDE = "keri.projectx.proxy.ClientProxy";
