84049563356fd02bad111a0c02a8bb28a4827eec
KitsuneAlex <kitsune-alex@gmx.de>
2017-06-23 17:01:43 +0200

    2.1.12

--- src/main/java/keri/projectx/util/ModPrefs.java
+++ src/main/java/keri/projectx/util/ModPrefs.java
@@ -10,7 +10,7 @@ public class ModPrefs {
 
     public static final String MODID = "projectx";
     public static final String NAME = "ProjectX 2";
-    public static final String VERSION = "2.1.11";
+    public static final String VERSION = "2.1.12";
     public static final String DEPS = "required-after:ninetaillib;after:waila";
     public static final String ACC_MC = "1.11.2";
     public static final String CSIDE = "keri.projectx.proxy.ClientProxy";
