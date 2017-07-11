ece238d2fcf5dd3686247b088a569725d0e5b6c6
KitsuneAlex <kitsune-alex@gmx.de>
2017-07-11 12:41:29 +0200

    Update copyright

--- src/main/java/keri/projectx/block/machine/BlockMulti.scala
+++ src/main/java/keri/projectx/block/machine/BlockMulti.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 KitsuneAlex. All rights reserved!
+ * Copyright (c) 2017 KitsuneAlex & Adam8234. All rights reserved!
  * Do not destribute or redistribute this software without the
  * explicit permission of the developer!
  */
--- src/main/java/keri/projectx/multiblock/MultiShadowTypes.java
+++ src/main/java/keri/projectx/multiblock/MultiShadowTypes.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 KitsuneAlex & Adam8234. All rights reserved!
+ * Do not destribute or redistribute this software without the
+ * explicit permission of the developer!
+ */
+
 package keri.projectx.multiblock;
 
 public enum MultiShadowTypes {
