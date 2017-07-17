0e301a17eaf42e55ac6d31877f59b27c77aa0ddb
KitsuneAlex <kitsune-alex@gmx.de>
2017-07-17 14:34:56 +0200

    Bump build number

--- src/main/scala/keri/projectx/util/ModPrefs.java
+++ src/main/scala/keri/projectx/util/ModPrefs.java
@@ -10,7 +10,7 @@ public class ModPrefs {
 
     public static final String MODID = "projectx";
     public static final String NAME = "ProjectX 2";
-    public static final String VERSION = "2.1.18";
+    public static final String VERSION = "2.1.19";
     public static final String DEPS =
             "required-after:minecraft;" +
             "required-after:ninetaillib;" +
